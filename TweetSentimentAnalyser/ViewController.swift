951515313f91dd14f238ce597f647d2f4adb3cf4
Michel Bueno <mbueno@thoughtworks.com>
2020-08-17 14:55:51 -0300

    Initial Commit

---
+++ TweetSentimentAnalyser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TweetSentimentAnalyser
+//
+//  Created by Michel Bueno on 17/08/20.
+//  Copyright © 2020 Michel Bueno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
