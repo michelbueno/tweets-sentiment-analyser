97e5e46535552ba186d7e2db7631390b3d03399c
Michel Bueno <mbueno@thoughtworks.com>
2020-08-20 14:47:43 -0300

    (WIP) Dinamically loading sentment score

--- TweetSentimentAnalyser/Classes/Modules/Tweets/TweetsViewController.swift
+++ TweetSentimentAnalyser/Classes/Modules/Tweets/TweetsViewController.swift
@@ -20,11 +20,13 @@ class TweetsViewController: UIViewController, TweetsViewControllerType {
 
     var username: String?
     var tweets: [String] = []
+    var cachedScore: [String: Double] = [:]
 
     override func loadView() {
         self.view = TweetsView()
         self.tableView.dataSource = self
         presenter?.loadView(with: username!)
+        self.navigationItem.title = "@\(username!)"
     }
 
     func loadTweets(_ tweetsList: [String]) {
@@ -47,12 +49,41 @@ extension TweetsViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let text = tweets[indexPath.row]
+        let score = cachedScore[text]
+
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "tweetCell")
-        cell.textLabel?.text = tweets[indexPath.row]
+        cell.textLabel?.text = text
         cell.backgroundColor = .darkGray
         cell.textLabel?.textColor = .white
         cell.detailTextLabel?.textColor = .white
         cell.textLabel?.numberOfLines = 0
+
+        if score == nil {
+            DispatchQueue.main.async {
+                let sentimentService = SentimentScoreService()
+                sentimentService.remoteService = RemoteService()
+                sentimentService.fetchSentimentScore(for: self.tweets[indexPath.row], onSuccess: {[unowned self] sentimentScore in
+                    self.cachedScore[text] = sentimentScore
+                    self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                }, onFailure: {
+                    cell.detailTextLabel?.text = "error"
+                })
+            }
+        } else {
+            switch score! {
+            case -1.0 ... -0.25:
+                cell.imageView?.image = UIImage(named: "sad")
+            case -0.25 ... 0.25:
+                cell.imageView?.image = UIImage(named: "neutral")
+            case 0.25 ... 1.0:
+                cell.imageView?.image = UIImage(named: "happy")
+            default:
+                cell.imageView?.image = UIImage(named: "neutral")
+            }
+        }
+
+
 
         return cell
     }
